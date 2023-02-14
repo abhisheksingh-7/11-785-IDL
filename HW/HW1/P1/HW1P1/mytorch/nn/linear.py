59b0c2c7014116053664a1f6ad0764cd82549ec5
Abhishek <abhisheksingh.dev71@gmail.com>
2023-02-13 23:02:57 -0500

    HW1P1 implemented linear layers

--- HW/HW1/P1/HW1P1/mytorch/nn/linear.py
+++ HW/HW1/P1/HW1P1/mytorch/nn/linear.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Linear:
 
     def __init__(self, in_features, out_features, debug=False):
@@ -9,8 +8,10 @@ class Linear:
         Checkout np.zeros function.
         Read the writeup to identify the right shapes for all.
         """
-        self.W = None  # TODO
-        self.b = None  # TODO
+        self.W = np.zeros((out_features, in_features))
+        assert self.W.shape == (out_features, in_features)
+        self.b = np.zeros(out_features)
+        assert self.b.shape == (out_features,)
 
         self.debug = debug
 
@@ -20,23 +21,37 @@ class Linear:
         :return: Output Z of linear layer with shape (N, C1)
         Read the writeup for implementation details
         """
-        self.A = None  # TODO
-        self.N = None  # TODO store the batch size of input
+        self.A = A
+        self.N = A.shape[0]  # store the batch size of input
         # Think how will self.Ones helps in the calculations and uncomment below
-        # self.Ones = np.ones((self.N,1))
-        Z = None  # TODO
+        self.Ones = np.ones((self.N,1))
 
-        return NotImplemented
+        # Z = (A . W^T) + (Ones . B^T)
+        Z = (self.A @ np.transpose(self.W)) + (self.Ones @ np.transpose(self.b))
+        assert Z.shape == (self.N, self.W.shape[0])
+
+        return Z
 
     def backward(self, dLdZ):
+        """
+        :param dLdZ: how changes in the layer's output affect loss
+        :return dLdA: how changes in the layer's inputs affect loss
+        """
+        assert dLdZ.shape == (self.A.shape[0], self.W.shape[0])
+        dZdA = np.transpose(self.W)
+        assert dZdA.shape == np.transpose(self.W).shape
+        dZdW = self.A
+        assert dZdW.shape == self.A.shape
+        dZdb = self.Ones
+        assert dZdb.shape == (self.A.shape[0],)
 
-        dZdA = None  # TODO
-        dZdW = None  # TODO
-        dZdb = None  # TODO
-
-        dLdA = None  # TODO
-        dLdW = None  # TODO
-        dLdb = None  # TODO
+        dLdA = dLdZ @ np.transpose(dZdA)
+        assert dLdA.shape == self.A.shape
+        dLdW = np.transpose(dLdZ) @ self.A
+        assert dLdW.shape == self.W.shape
+        dLdb = np.transpose(dLdZ) @ dZdb
+        assert dLdb.shape == (self.W.shape[0],)
+        
         self.dLdW = dLdW / self.N
         self.dLdb = dLdb / self.N
 
@@ -47,4 +62,4 @@ class Linear:
             self.dZdb = dZdb
             self.dLdA = dLdA
 
-        return NotImplemented
+        return dLdA
